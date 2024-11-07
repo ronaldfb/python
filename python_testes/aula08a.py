e1841f091f4be04b33dbc84905cd3511466c0ba6
Ronald <ronaldferrazb@gmail.com>
2024-11-07 14:09:40 -0300

    .

---
+++ python_testes/aula08a.py
@@ -0,0 +1,8 @@
+# #import math
+# from math import sqrt
+# num = int(input('Digite um número: '))
+# #raiz = math.sqrt(num)
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+import emoji
+print(emoji.emojize('Olá, mundo :England: :fish:'))
