7412a26210829063904dea1bb8f3bedd85b6612a
Ronald <ronaldferrazb@gmail.com>
2025-02-14 15:05:59 -0300

    Create desafio38.py

---
+++ desafios/desafio38.py
@@ -0,0 +1,8 @@
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+if num1 > num2:
+    print('O PRIMEIRO valor é maior')
+elif num2 > num1:
+    print('O SEGUNDO valor é maior')
+else:
+    print('Os dois valores são IGUAIS')
