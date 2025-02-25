c3067ebf38659ef5e6aa31049a8347f79a62f7e1
Ronald <ronaldferrazb@gmail.com>
2025-02-25 13:36:09 -0300

    ...

--- desafios/desafio46.py
+++ desafios/desafio46.py
@@ -1,6 +1,6 @@
 from time import sleep
 print('Contagem regressiva para o estouro dos fogos de artifício.')
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
 print('Estourando os fogos!')
