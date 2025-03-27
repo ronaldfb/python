2ce718555c7a143cf3e3fba8e84aac2a08a77c24
Ronald <ronaldferrazb@gmail.com>
2025-03-27 14:39:49 -0300

    Update desafio56.py

--- desafios/desafio56.py
+++ desafios/desafio56.py
@@ -8,11 +8,11 @@ for p in range(1, 5):
     idade = int(input('Idade: '))
     sexo = str(input('[M/F]: ')).upper()
     totidade += idade
-    if sexo == 'F'and idade < 20:
-       totmulher += 1
     if sexo == 'M' and idade > homemvelho:
         homemvelho = idade
         nomemaisvelho = nome  
+    if sexo == 'F'and idade < 20:
+       totmulher += 1
         
 mediaidade = totidade/4
 print('-------------------------------------------------------------')
