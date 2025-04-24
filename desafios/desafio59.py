d6969b48ba104587f584256ea4b28cc7cc67e9d4
Ronald <ronaldferrazb@gmail.com>
2025-04-24 11:01:28 -0300

    Update desafio59.py

--- desafios/desafio59.py
+++ desafios/desafio59.py
@@ -4,6 +4,7 @@ v2 = int(input('Segundo valor: '))
 sair = False
 while not sair:
     sleep(1.2)
+    print('-='*16)
     print('[ 1 ] somar\n[ 2 ] multiplicar\n[ 3 ] maior\n[ 4 ] novos números\n[ 5 ] sair do programa')
     opcao = int(input('>>>>>>> Qual é sua opção? '))
     if opcao == 5:
@@ -21,11 +22,12 @@ while not sair:
         if v1 == v2:
             print('Os dois valores são iguais!')
         elif v1 > v2:
-            print('O valor {} é maior que {}'.format(v1,v2))
+            print('Entre {} e {} o maior valor é {}'.format(v1,v2,v1))
         else:
-            print('O valor {} é maior que {}'.format(v2,v1))
+            print('Entre {} e {} o maior valor é {}'.format(v1,v2,v2))
     elif opcao == 4:
         sleep(0.5)
+        print('Informe os números novamente:')
         v1 = int(input('Primeiro valor: '))
         v2 = int(input('Segundo valor: '))
 sleep(1)
