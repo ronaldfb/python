2a6b2206f49437a11164258d04d5c5bf06772b1c
Ronald <ronaldferrazb@gmail.com>
2025-04-25 13:59:18 -0300

    Update desafio61.py

--- desafios/desafio61.py
+++ desafios/desafio61.py
@@ -2,9 +2,27 @@ print('Gerador de PA')
 print('=-='*8)
 pt = int(input('Primeiro termo: '))
 r = int(input('Razão da PA: '))
-c = 0
-while c < 10:
-    print('{}'.format(pt), end=' → ')
-    pt += r
+termo = pt
+c = 1
+while c <= 10:
+    print('{} → '.format(termo), end='')
+    termo = termo + r
     c += 1
-print('Fim')
+print('FIM')
+
+
+
+
+
+
+
+# print('Gerador de PA')
+# print('=-='*8)
+# pt = int(input('Primeiro termo: '))
+# r = int(input('Razão da PA: '))
+# c = 1
+# while c <= 10:
+#     print('{}'.format(pt), end=' → ')
+#     pt += r
+#     c += 1
+# print('Fim')
