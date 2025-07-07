cf7f39339a1a6341dd85a9eead77b8206192fc29
Ronald <ronaldferrazb@gmail.com>
2025-07-07 15:20:36 -0300

    Update desafio71.py

--- desafios/desafio71.py
+++ desafios/desafio71.py
@@ -2,18 +2,24 @@
 print('='*20)
 print('   BANCO FERRAZ')
 print('='*20)
+valor = int(input('Qual valor vai ser sacado? R$'))
+total = valor
+céd = 50
+totcéd = 0
 while True:
-    valor = float(input('Quanto quer sacar? R$'))
-    
-    cedulas_50 = valor // 50 #Quantas vezes 50 cabe dentro do valor
-    resto = valor % 50 #Resto da divisão
-    
-    cedulas_10 = resto // 10 #Quantas vezes 10 cabe no restante da divisão anterior
-    resto = resto % 10 #Resto da divisão
-    
-    cedulas_1 = resto
-    break
-print(f'Total de {cedulas_50:.0f} cédulas de R$50')
-print(f'Total de {cedulas_10:.0f} cédulas de R$10')
-print(f'Total de {cedulas_1:.0f} cédulas de R$1')
-    
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cedulas de R${céd}')
+        
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+         break
