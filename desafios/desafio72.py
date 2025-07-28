c4791ae43df1a1cdcaadd6ce2dcfc273464ca754
Ronald <ronaldferrazb@gmail.com>
2025-07-28 14:19:54 -0300

    Update desafio72.py

--- desafios/desafio72.py
+++ desafios/desafio72.py
@@ -5,8 +5,18 @@ numext = ('zero', 'um', 'dois', 'três', 'quatro',
           'dezenove', 'vinte')
 while True:
     num = int(input('Digite um número entre 0 e 20: '))
-    if 0 <= num <= 20:
+    while num <0 or num > 20:
+        print('Tente novamente. ', end='')
+        num = int(input('Digite um número entre 0 e 20: '))
+    if num >= 0 and num <= 20:
+        print(f'Você digitou o número {numext[num]}')
+
+         
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
         break
-    print('Tente novamente.', end='')
-print(f'Você digitou o número {numext[num]}')
+
+print('Fim do programa! ')
     
