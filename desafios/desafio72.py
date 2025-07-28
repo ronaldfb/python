03524e317aedd551553f4fe74a400f6c4122f983
Ronald <ronaldferrazb@gmail.com>
2025-07-28 14:21:35 -0300

    Update desafio72.py

--- desafios/desafio72.py
+++ desafios/desafio72.py
@@ -9,9 +9,7 @@ while True:
         print('Tente novamente. ', end='')
         num = int(input('Digite um número entre 0 e 20: '))
     if num >= 0 and num <= 20:
-        print(f'Você digitou o número {numext[num]}')
-
-         
+        print(f'Você digitou o número {numext[num]}')     
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
