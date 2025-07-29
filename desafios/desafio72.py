b2ea9fd5991e5732e76ed8008514b72cf8806cfb
Ronald <ronaldferrazb@gmail.com>
2025-07-29 10:48:23 -0300

    Update desafio72.py

--- desafios/desafio72.py
+++ desafios/desafio72.py
@@ -1,19 +1,30 @@
-numext = ('zero', 'um', 'dois', 'três', 'quatro', 
-          'cinco', 'seis', 'sete', 'oito', 'nove', 
-          'dez', 'onze', 'doze', 'treze', 'quatorze', 
-          'quinze', 'dezesseis', 'dezessete', 'dezoito', 
-          'dezenove', 'vinte')
+numext = (
+    'zero', 'um', 'dois', 'três', 'quatro', 
+    'cinco', 'seis', 'sete', 'oito', 'nove', 
+    'dez', 'onze', 'doze', 'treze', 'quatorze', 
+    'quinze', 'dezesseis', 'dezessete', 'dezoito', 
+    'dezenove', 'vinte'
+)
+
 while True:
-    num = int(input('Digite um número entre 0 e 20: '))
-    while num <0 or num > 20:
-        print('Tente novamente. ', end='')
-        num = int(input('Digite um número entre 0 e 20: '))
-    if num >= 0 and num <= 20:
-        print(f'Você digitou o número {numext[num]}')     
-    resp = ' '
-    while resp not in 'SN':
-        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while True:
+        try:
+            num = int(input('Digite um número entre 0 e 20: '))
+            if 0 <= num <= 20:
+                break
+            else:
+                print('Valor fora do intervalo. Tente novamente.')
+        except ValueError:
+            print('Entrada inválida. Digite um número inteiro.')
+
+    print(f'Você digitou o número {numext[num]}.')
+
+    resp = input('Quer continuar? [S/N] ').strip().upper()
+    while resp not in ('S', 'N'):
+        resp = input('Resposta inválida. Quer continuar? [S/N] ').strip().upper()
+    
     if resp == 'N':
         break
-print('Fim do programa! ')
+
+print('Fim do programa!')
     
