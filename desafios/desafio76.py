20acd3de645872ab8998896889203c1aab89a1cd
Ronald <ronaldferrazb@gmail.com>
2025-07-31 13:14:40 -0300

    Update desafio76.py

--- desafios/desafio76.py
+++ desafios/desafio76.py
@@ -12,5 +12,5 @@ for pos in range(0, len(listagem)):
     if pos % 2 == 0:
         print(f'{listagem[pos]:.<30}', end='')
     else:
-        print(f'R${listagem[pos]:>7}')
+        print(f'R${listagem[pos]:>7.2f}')
     
