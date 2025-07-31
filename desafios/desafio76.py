9fd19647ddcf1ccdc763abe8b64f4a7cef8932f7
Ronald <ronaldferrazb@gmail.com>
2025-07-31 13:16:31 -0300

    Update desafio76.py

--- desafios/desafio76.py
+++ desafios/desafio76.py
@@ -5,9 +5,9 @@ listagem = ('Borracha', 2,
             'Mochila', 59.99,
             'Estojo', 11.45,
             'Livro', 30)
-print('-'*30)
-print('LISTAGEM DE PREÇOS')
-print('-'*30)
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
 for pos in range(0, len(listagem)):
     if pos % 2 == 0:
         print(f'{listagem[pos]:.<30}', end='')
